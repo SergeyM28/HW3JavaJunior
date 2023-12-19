c492f4aeabe8a229c39ec1e0823a2108e229f217
SergeiM <mihailovsv@mail.ru>
2023-12-19 20:57:47 +0300

    HW 3 Java Junior by Mikhailov

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,31 @@
-package org.example;public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+package org.example;
+
+//Разработайте класс Student с полями String name, int age, transient double GPA (средний балл).
+//Обеспечьте поддержку сериализации для этого класса. Создайте объект класса Student и инициализируйте его данными.
+//Сериализуйте этот объект в файл. Десериализуйте объект обратно в программу из файла. Выведите все поля объекта,
+//включая GPA, и обсудите, почему значение GPA не было сохранено/восстановлено.
+
+import java.io.*;
+
+//2. * Выполнить задачу 1 используя другие типы сериализаторов (в xml и json документы).
+public class Main {
+    public static void main(String[] args) throws IOException, ClassNotFoundException {
+        Student student = new Student("Harry Potter", 13, 4.5);
+
+        try(FileOutputStream fileOutputStream = new FileOutputStream("students.txt")) {
+            ObjectOutputStream objectOutputStream = new ObjectOutputStream(fileOutputStream);
+            objectOutputStream.writeObject(student);
+            System.out.println(student.getName() + " успешно сериализован");
+        }
+
+        try(FileInputStream fileInputStream = new FileInputStream("students.txt")){
+            ObjectInputStream objectInputStream = new ObjectInputStream(fileInputStream);
+            Student student2 = (Student) objectInputStream.readObject();
+            System.out.println(student2.getName() + " успешно десериализован");
+            System.out.println(student2); // поле GPA помечено transient, поэтому оно не было сохранено и не было восстановлено
+        }
+
+
+
     }
 }
--- src/main/java/org/example/Student.java
+++ src/main/java/org/example/Student.java
@@ -1,2 +1,36 @@
-package org.example;public class Student {
+package org.example;
+
+import java.io.Serializable;
+
+public class Student implements Serializable {
+    private String name;
+    private int age;
+    transient double GPA; //Средний балл
+
+    public Student(String name, int age, double GPA) {
+        this.name = name;
+        this.age = age;
+        this.GPA = GPA;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public double getGPA() {
+        return GPA;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", GPA=" + GPA +
+                '}';
+    }
 }
